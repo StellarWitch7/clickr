91ba40618dd72edf2f24b99e910ed7b5441c2f75
enjarai <enjarai@trans.fish>
2025-02-13 22:05:24 +0100

    Meowmeow!!

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use std::{io::{Read, Write}, net::Shutdown, os::unix::net::{UnixListener, UnixStream}, sync::Mutex, thread::spawn};
+use std::{io::{Read, Write}, net::Shutdown, os::unix::net::{UnixListener, UnixStream}, process, sync::Mutex, thread::spawn, time::Duration};
 
-use actix_web::{get, web::{self, Data, Payload}, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web::{get, rt::time::sleep, web::{self, Data, Payload}, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_ws::Session;
+use awc::Client;
 use clap::{command, Parser, Subcommand, ValueEnum};
 use env_logger::Target;
-use futures::executor::block_on;
-use log::{error, info, LevelFilter};
+use futures::{executor::block_on, StreamExt};
+use log::{error, info, warn, LevelFilter};
 
 #[derive(Debug, Parser)]
 #[command(name = "clickr")]
@@ -47,11 +48,7 @@ async fn main() {
 async fn run() -> Result<(), String> {
     match Cli::parse().command {
         Command::Ping => {
-            //TODO: connect to ~/.config/clickr/sock as a unix socket
-            //send a single byte
-            //disconnect and terminate
-
-            let mut stream = UnixStream::connect(shellexpand::tilde("~/.config/clickr/sock"))
+            let mut stream = UnixStream::connect("~/.config/clickr/sock")
                 .or_else(|e| Err(format!("Failed to open unix socket: {e}")))?;
             stream.write_all(&[0xff])
                 .or_else(|e| Err(format!("Failed to write to unix socket: {e}")))?;
@@ -63,13 +60,36 @@ async fn run() -> Result<(), String> {
             Ok(())
         },
         Command::Connect(args) => {
-            //TODO: connect to ws://{args.addr}:{args.port}/heart
-            //stay running
-            //if disconnected, attempt to reconnect
-            //when receiving a message through the websocket, play the audio file at
-            //~/.config/clickr/sound using pw-cat
-            //use awc::ws for client websocket
-            Ok(())
+            let client = Client::default();
+
+            loop {
+                let (res, mut ws) = client
+                    .ws(format!("ws://{}:{}/heart", args.addr, args.port))
+                    .connect()
+                    .await
+                    .or_else(|e| Err(format!("Failed to connect to websocket: {e}")))?;
+                info!("Connected! Http response: {res:?}");
+
+                loop {
+                    match ws.next().await {
+                        Some(msg) => {
+                            if let Ok(msg) = msg {
+                                info!("Got ping! {msg:?}");
+                                process::Command::new("pw-cat")
+                                    .arg("-p")
+                                    .arg("~/.config/clickr/sound.ogg")
+                                    .output()
+                                    .or_else(|e| Err(format!("Failed to execute pw-cat: {e}")))?;
+                            }
+                        },
+                        None => {
+                            warn!("Got disconnected! Attempting to reconnect in 5 seconds.");
+                            sleep(Duration::from_secs(5)).await;
+                            break;
+                        }
+                    }
+                }
+            }
         },
         Command::Host(args) => {
             let Args { addr, port } = args.clone();
@@ -80,11 +100,7 @@ async fn run() -> Result<(), String> {
             })
                 .bind((addr, port))
                 .or_else(|e| Err(format!("Failed to bind to address: {e}")))?;
-
-            //TODO: connect to ~/.config/clickr/sock as a unix socket
-            //listen for a byte input into the socket, then send a byte message over the currently
-            //connected websocket
-            //do this on a separate thread
+            
             info!("Setting up socket listener thread...");
             let listener = UnixListener::bind(shellexpand::tilde("~/.config/clickr/sock"))
                 .or_else(|e| Err(format!("Failed to bind unix socket: {e}")))?;
@@ -112,9 +128,6 @@ async fn run() -> Result<(), String> {
 #[get("/heart")]
 pub async fn heart(req: HttpRequest, stream: Payload, args: Data<Args>) -> Result<HttpResponse, Error> {
     let (res, session, stream) = actix_ws::handle(&req, stream)?;
-    //TODO: pass `session` as connection
-    //disconnect the old session if there is one. must be thread-safe. separate thread should
-    //handle reading from the unix socket and sending pings through the websocket session
 
     let mut current_session = get_client_session().lock().unwrap();
     if current_session.as_ref().is_some() {
